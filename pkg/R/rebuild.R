738e2eef2d3b6fb562e6bcf329387c7f7977971b
jombart (Mr ) <t.jombart@imperial.ac.uk>
2009-02-06 17:59:05 +0000

    Added a first draft of rebuild function. May have to make it a generic later. Have to make it work first. Yiiiipeee !
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geograph@24 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/rebuild.R
@@ -0,0 +1,41 @@
+###########
+# rebuild
+###########
+rebuild <- function(x, upTo){
+    ## misc checks
+    if(!is.gGraph(x)) stop("x is not a valid gGraph object")
+
+
+    ## misc variables
+    myHistory <- getHistory(x)
+    L <- length(myHistory@cmd)
+    myDates <- getDates(x)
+
+
+    ## handle upTo if character
+    if(is.character(upTo)){
+         upTo <- as.POSIXct(upTo)
+         toKeep <- (myDates < upTo)
+    } else if(is.numeric(upTo)){
+        upTo <- round(upTo)
+        if(upTo > L) stop("upTo cannot be greater than the number of items in @history")
+        if(upTo == L) {
+            warning("Pointless operation.")
+            return(x)
+        }
+
+        toKeep <- 1:upTo
+    }
+
+
+    ## handle history
+    myHistory <- myHistory[toKeep]
+    myCmd <- myHistory@cmd
+
+    for(i in 1:upTo){
+        res <- eval(myCmd[[i]])
+    }
+
+    return(res)
+
+} # end rebuild
