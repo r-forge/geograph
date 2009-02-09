aa4cf834214c2892efb7f98ff1ce9e5dd06fac5d
jombart (Mr ) <t.jombart@imperial.ac.uk>
2009-02-09 19:32:54 +0000

    Fixes to plots.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geograph@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,10 +1,19 @@
 .First.lib <- function (lib, pkg){
-##.initAdegenetClasses()
-##.initAdegenetUtils()
+    ##.initAdegenetClasses()
+    ##.initAdegenetUtils()
     ## library.dynam("adegenet", pkg, lib)
 
-    cat("\n\t====================\n")
-    cat("\t geoGraph is loaded ")
-    cat("\n\t====================\n")
+    cat("\n\t========================\n")
+    cat("\t geoGraph 1.0-0 is loaded ")
+    cat("\n\t========================\n")
+
+    ## define various items in our specific environment
     assign(".geoGraphEnv", new.env(parent=.GlobalEnv), env=.GlobalEnv)
+    geoEnv <- get(".geoGraphEnv", envir=.GlobalEnv)
+
+    zoom.log <- matrix(c(-180,180,-90,90),ncol=4)
+    colnames(zoom.log) <- c("x1","x2","y1","y2")
+
+    assign("zoom.log", zoom.log, env=geoEnv)
+    assign("psize", 2, env=geoEnv)
 }
