c68e7578a91180470139d35ad9d69274d9cc3c97
jombart (Mr ) <t.jombart@imperial.ac.uk>
2009-02-10 19:42:44 +0000

    Improve isInArea: now can use user-supplied boundaries.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geograph@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/aux.R
+++ pkg/R/aux.R
@@ -1,34 +1,44 @@
 ############
 ## isInArea
 ############
-isInArea <- function(x, buffer=0){
+isInArea <- function(x, reg="current", buffer=0){
     ## some checks
     if(!is.gGraph(x)) stop("x is not a valid gGraph object")
 
     env <- get(".geoGraphEnv", envir=.GlobalEnv) # env is our target environnement
     coords <- getCoords(x)
 
-    ## main computations
-    if(exists("zoom.log", envir=env)){
+    ## get xlim and ylim
+    if(exists("zoom.log", envir=env) & reg=="current"){ # xlim/ylim taken from log
         zoomlog <- get("zoom.log", envir=env)
         zoomlog <- zoomlog[1,]
 
         xlim <- zoomlog[1:2]
         ylim <- zoomlog[3:4]
 
-        ## handle a buffer around area
-        bufferx <- (xlim[2]-xlim[1])*buffer
-        buffery <- (ylim[2]-ylim[1])*buffer
+    } else if(is.list(reg)){ # xlim/ylim user-provided (reg)
+        if(length(reg)!=2) stop("reg is not a list of length 2.")
+        xlim <- sort(reg[[1]])[1:2]
+        ylim <- sort(reg[[2]])[1:2]
 
-        xlim <- xlim + c(-bufferx, bufferx)
-        ylim <- ylim + c(-buffery, buffery)
+    } else return(NA)
 
 
-        toKeep <- ( (coords[,1] >= xlim[1]) & (coords[,1] <= xlim[2])  # matching longitude
-                   & (coords[,2] >= ylim[1]) & (coords[,2] <= ylim[2]) ) # matching latitude
+    ## main computations ##
 
-        names(toKeep) <- rownames(coords)
-        return(toKeep)
-    } else return(TRUE)
+    ## handle a buffer around area
+    bufferx <- (xlim[2]-xlim[1])*buffer
+    buffery <- (ylim[2]-ylim[1])*buffer
+
+    xlim <- xlim + c(-bufferx, bufferx)
+    ylim <- ylim + c(-buffery, buffery)
+
+    toKeep <- ( (coords[,1] >= xlim[1]) & (coords[,1] <= xlim[2])  # matching longitude
+               & (coords[,2] >= ylim[1]) & (coords[,2] <= ylim[2]) ) # matching latitude
+
+    names(toKeep) <- rownames(coords)
+
+    return(toKeep)
+
 
 } # end isInArea
