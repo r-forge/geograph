3aea55f2eba99e828d263ae2dbba28acfa12162e
jombart (Mr ) <t.jombart@imperial.ac.uk>
2009-02-11 12:30:34 +0000

    Forgot the file. Interact will contain functions allowing one to modify the content of an object interactively, via graphics.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geograph@52 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/interact.R
@@ -0,0 +1,45 @@
+#################
+## geo.add.edges
+#################
+geo.add.edges <- function(x) {
+    ## preliminary stuff
+    if(!is.gGraph(x)) stop("x is not a valid gGraph object")
+    temp <- isInArea(x)
+    coords <- getCoords(x)[temp,]
+    nodes <- getNodes(x)[temp]
+    lon <- coords[,1]
+    lat <- coords[,2]
+    env <- get(".geoGraphEnv", envir=.GlobalEnv) # env is our target environnement
+    psize <- get("psize", env=env)
+
+    toAdd <- list(from, to)
+    spoint <- 1:2
+
+    while (length(spoint)>1) {
+      spoint <- NULL
+      spoint <- identify(lon, lat, plot=FALSE, n=2)
+      if(length(spoint)>1) {
+        segments(lon[spoint[1]], lat[spoint[1]], lon[spoint[2]], lat[spoint[2]], col="red")
+        points(lon[spoint[1]],lat[spoint[1]],cex=psize)
+        points(lon[spoint[2]],lat[spoint[2]],cex=psize)
+
+        toAdd$from <- c(toAdd$from, nodes[spoint[1]])
+        toAdd$to <- c(toAdd$from, nodes[spoint[2]])
+      }
+    }
+
+    ## make sure added edges are unique
+    toAdd <- as.matrix(as.data.frame(toAdd))
+    toAdd <- t(apply(toAdd,1,sort)) # sorting
+    toAdd <- paste(toAdd[,1], toAdd[,2], sep="-") # making strings
+    toAdd <- unique(toAdd) # keep unique strings
+    toAdd <- strsplit(toAdd, "-")
+    from <- sapply(toAdd, function(e) e[1])
+    to <- sapply(toAdd, function(e) e[2])
+
+    ## build res
+    res <- setEdges(x=x, add=cbind(from, to) )
+
+    return(res)
+} # end geo.add.edges
+
