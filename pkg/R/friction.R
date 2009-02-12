a86075a55b8cbdc6b9b7cbe2f8b3eff2a234b826
jombart (Mr ) <t.jombart@imperial.ac.uk>
2009-02-12 16:41:41 +0000

    'commit' takes its sense with this first draft of setFriction. Have to test & debug it.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geograph@68 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/friction.R
+++ pkg/R/friction.R
@@ -1,9 +1,46 @@
-###########
-## fiction
-###########
-fiction <- function(x){
-    ## some checks
+###############
+## setFriction
+###############
+friction <- function(x, attr.name=NULL, method=c("mean", "product"), drop=TRUE){
+    ## some checks + argument handling
     if(!is.gGraph(x)) stop("x is not a valid gGraph object")
+    method <- match.arg(method)
 
 
-}
+    ## assign weights to vertices
+    nodeAttr <- getNodesAttr(x, attr.name=attr.name)
+    if(!is.null(x@meta$weights)){
+        if(!any(attr.name %in% colnames(x@meta$weights))) {
+            stop("attr.name is not documented in x@meta$weights.")
+        }
+        nodeWeights <- nodeAttr
+        rules <- x@meta$weights
+        for(i in 1:nrow(x@meta$weights)){
+            nodeWeights[nodeWeights==rules[i,attr.name]] <- rules[i,"weight"]
+        }
+    } else stop("x@meta does not contain a 'weights' component.")
+
+    ## find weights of edges as a function of terminating vertices
+    EL <- getGraph(x)@edgeL
+
+    ## method == mean ##
+    if(method=="mean"){
+        for(i in 1:length(EL)){
+            EL[[i]]$weights <- (nodeWeights[i] + nodeWeights[EL[[i]]$edges]) / 2
+            }
+    }
+
+    ## method == product ##
+    if(method=="product"){
+        for(i in 1:length(EL)){
+            EL[[i]]$weights <- nodeWeights[i] * nodeWeights[EL[[i]]$edges]
+            }
+    }
+
+    ## return result
+    newGraph <- new("graphNEL", nodes=nodes(x), edgeL=EL)
+    res <- x
+    res@graph <- newGraph
+
+    return(res)
+} # end friction
