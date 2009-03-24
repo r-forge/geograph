6cf4a618f61f1bc0b202347ecd3891bc092feb41
jombart (Mr ) <t.jombart@imperial.ac.uk>
2009-03-24 18:35:27 +0000

    added a new option to setFriction allowing node costs to be user-spec.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geograph@277 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/friction.R
+++ pkg/R/friction.R
@@ -1,25 +1,32 @@
 ###############
 ## setFriction
 ###############
-setFriction <- function(x, attr.name=NULL, method=c("mean", "product")){
+setFriction <- function(x, attr.name=NULL, node.costs=NULL, method=c("mean", "product")){
     ## some checks + argument handling
     if(!is.gGraph(x)) stop("x is not a valid gGraph object")
     method <- match.arg(method)
 
 
     ## assign costs to vertices
-    nodeAttr <- unlist(getNodesAttr(x, attr.name=attr.name))
-    if(!is.null(x@meta$costs)){
-        if(!any(attr.name %in% colnames(x@meta$costs))) {
-            stop("attr.name is not documented in x@meta$costs.")
-        }
-        nodeCosts <- as.character(nodeAttr)
-        rules <- x@meta$costs
-        for(i in 1:nrow(x@meta$costs)){
-            nodeCosts[nodeCosts==rules[i,attr.name]] <- rules[i,ncol(rules)]
-        }
-        nodeCosts <- as.numeric(nodeCosts)
-    } else stop("x@meta does not contain a 'costs' component.")
+    if(is.null(costs)){ # costs from a node attribute
+        nodeAttr <- unlist(getNodesAttr(x, attr.name=attr.name))
+        if(!is.null(x@meta$costs)){
+            if(!any(attr.name %in% colnames(x@meta$costs))) {
+                stop("attr.name is not documented in x@meta$costs.")
+            }
+            nodeCosts <- as.character(nodeAttr)
+            rules <- x@meta$costs
+            for(i in 1:nrow(x@meta$costs)){
+                nodeCosts[nodeCosts==rules[i,attr.name]] <- rules[i,ncol(rules)]
+            }
+            nodeCosts <- as.numeric(nodeCosts)
+        } else stop("x@meta does not contain a 'costs' component.")
+    } else{ # cost directly provided
+        if(!is.numeric(node.costs)) stop("Provided 'node.costs' not numeric.")
+        node.costs <- rep(node.costs, length=length(getNodes(x))) # recycling node costs
+        nodeCosts <- node.costs
+    }
+
 
     ## find costs of edges as a function of terminating vertices
     EL <- getGraph(x)@edgeL
