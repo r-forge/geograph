944bf651a3ecbad3e9615e0f3176b2e2d087d83b
jombart (Mr ) <t.jombart@imperial.ac.uk>
2009-04-08 23:20:40 +0000

    gpp data + their integration to worldgraph.40k
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geograph@304 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/misc/productivity/processInfo.R
@@ -0,0 +1,30 @@
+##
+## code to process monthly_gpp.out for geoGraph
+##
+
+
+## ! read.table won't read it all
+txt <- scan("/home/master/dev/geograph/pkg/misc/productivity/monthly_gpp.out",what="numeric")
+txt <- as.numeric(txt)
+
+gpp <- matrix(txt, byrow=TRUE, ncol=4)
+gpp <- as.data.frame(gpp)
+names(gpp) <- c("lat","lon","month","gpp")
+## myNodes <- closestNode(rawgraph.40k, gpp[,2:1]) # this takes ages
+
+## save gpp
+gpp$node <- myNodes
+save(gpp, file="/home/master/dev/geograph/pkg/misc/productivity/gpp.RData")
+
+
+## mean prod, var, range size
+meanProd <- tapply(gpp$gpp, gpp$node, mean)
+varProd <- tapply(gpp$gpp, gpp$node, var)
+## rangeProd <- tapply(gpp$gpp, gpp$node, function(e) diff(range(e)))
+## minProd <- tapply(gpp$gpp, gpp$node, min)
+
+
+data(worldgraph.40k)
+worldgraph.40k@nodes.attr <- cbind.data.frame(worldgraph.40k@nodes.attr, meanProd, varProd)
+
+## save(worldgraph.40k, file="/home/master/dev/geograph/pkg/data/worldgraph.40k.RData")
